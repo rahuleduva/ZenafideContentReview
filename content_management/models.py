2c59ced6b8341207dcd4f72abdc41592e88eab25
rahuleduva <rahul.eduva@gmail.com>
2024-03-18 22:47:49 +0530

    Model file validators

--- content_management/models.py
+++ content_management/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 from user_management.models import User
 
 # Create your models here.
@@ -6,8 +7,10 @@ from user_management.models import User
 class Guideline(models.Model):
     tag = models.CharField(max_length=100)
     description = models.TextField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='guidelines_created', null=True)
-    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='guidelines_updated')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+                                   related_name='guidelines_created',)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                   related_name='guidelines_updated')
 
 
 class Content(models.Model):
@@ -17,14 +20,21 @@ class Content(models.Model):
         ('FAILED', 'Failed'),
     )
     title = models.CharField(max_length=200)
-    content_file = models.FileField(upload_to='author_content_files')
+    content_file = models.FileField(upload_to='author_content_files', validators=[
+                FileExtensionValidator(
+                    ['txt', 'png', 'jpg', 'jpeg', 'svg', 'pdf', 'docx', 'odf',]
+                )
+            ]
+        )
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     is_submitted = models.BooleanField(default=False)
-    status = models.CharField(choices=CONTENT_APPROVAL_CHOICES, max_length=15, default='NOT_REVIEWED')
+    status = models.CharField(choices=CONTENT_APPROVAL_CHOICES,
+                              max_length=15, default='NOT_REVIEWED')
 
 
 class ContentGuidelineApproval(models.Model):
     guideline = models.ForeignKey(Guideline, on_delete=models.CASCADE)
-    content = models.ForeignKey(Content, on_delete=models.CASCADE, related_name='content_guidelines_actions')
+    content = models.ForeignKey(Content, on_delete=models.CASCADE,
+                                related_name='content_guidelines_actions')
     acted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     is_approved = models.BooleanField(default=True)
