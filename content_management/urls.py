c45d57bb9a823b96ed7de47961fce84af6494162
rahuleduva <rahul.eduva@gmail.com>
2024-03-19 04:36:30 +0530

    Re-implemented with DRF ModelViewSets

--- content_management/urls.py
+++ content_management/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from content_management.views import (
     ListCreateGuideline,
     RetrieveUpdateDestroyGuideline,
@@ -6,7 +6,17 @@ from content_management.views import (
     RetrieveUpdateDestroyContent,
     ContentGuidelineBulkApproval,
     ContentGuidelinesBulkApprovalV2,
+
+    GuidelineViewSet,
+    ContentViewSet,
+    ContentGuidelinesBulkApprovalViewset,
 )
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'guidelines', GuidelineViewSet, basename='guideline')
+router.register(r'contents', ContentViewSet, basename='content')
+router.register(r'contentGuidelinesBulkApprovalsViewset', ContentGuidelinesBulkApprovalViewset, basename='contentGuidelinesBulkApproval')
 
 urlpatterns = [
     path('listCreateGuideline/', ListCreateGuideline.as_view(), name='listCreateGuideline'),
@@ -15,4 +25,8 @@ urlpatterns = [
     path('retrieveUpdateDeleteContent/<int:pk>', RetrieveUpdateDestroyContent.as_view(), name='retrieveUpdateDeleteContent'),
     path('contentGuidelineBulkApproval/', ContentGuidelineBulkApproval.as_view(), name='contentGuidelineBulkApproval'),
     path('contentGuidelinesBulkApprovalV2/', ContentGuidelinesBulkApprovalV2.as_view(), name='contentGuidelinesBulkApprovalV2'),
+]
+
+urlpatterns += [
+    path('', include(router.urls)),
 ]
--- content_management/views.py
+++ content_management/views.py
@@ -7,6 +7,10 @@ from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView,
     CreateAPIView,
 )
+from rest_framework.viewsets import (
+    GenericViewSet, ViewSet, ViewSetMixin, ModelViewSet,
+    )
+from rest_framework.mixins import CreateModelMixin
 from django_filters import rest_framework as filters
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django.db.models import Q
@@ -44,6 +48,18 @@ class RetrieveUpdateDestroyGuideline(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsComplianceUserOrReadOnly]
 
 
+class GuidelineViewSet(ModelViewSet):
+    serializer_class = GuidelineListCreateSerializer
+    queryset = Guideline.objects.all()
+    permission_classes = [IsComplianceUserOrReadOnly]
+
+    def get_serializer_class(self):
+        sl = self.serializer_class
+        if self.kwargs.get('pk'):
+            return GuidelineRetrieveUpdateSerializer
+        return sl
+
+
 class ListCreateContent(ListCreateAPIView):
     serializer_class = ContentListCreateSerializer
     queryset = Content.objects.all().prefetch_related()
@@ -66,10 +82,40 @@ class ListCreateContent(ListCreateAPIView):
             qs = qs.filter(status='PASSED')
         return qs
 
+
 class RetrieveUpdateDestroyContent(RetrieveUpdateDestroyAPIView):
     serializer_class = ContentRetrieveUpdateSerializer
     queryset = Content.objects.all()
     permission_classes = [IsContentCreatorOrReadOnly]
+
+
+class ContentViewSet(ModelViewSet):
+    serializer_class = ContentListCreateSerializer
+    queryset = Content.objects.all()
+    permission_classes = [IsContentCreatorOrReadOnly]
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter,)
+    filterset_fields = ['is_submitted', 'status']
+    search_fields = ['title']    
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user = self.request.user
+        if user.is_authenticated and user.user_type in ('author', 'reviewer'):
+            if user.user_type == 'author':
+                qs = qs.exclude(
+                    ~Q(status='PASSED') & ~Q(created_by=user)
+                )
+            elif user.user_type == 'compliance_user':
+                qs = qs.filter(is_submitted=True)
+        else:
+            qs = qs.filter(status='PASSED')
+        return qs
+
+    def get_serializer_class(self):
+        sl = self.serializer_class
+        if self.kwargs.get('pk'):
+            return ContentRetrieveUpdateSerializer
+        return sl
 
 
 class ContentGuidelineBulkApproval(ListCreateAPIView):
@@ -86,6 +132,16 @@ class ContentGuidelineBulkApproval(ListCreateAPIView):
 class ContentGuidelinesBulkApprovalV2(CreateAPIView):
     permission_classes = [IsReviewer]
     serializer_class = ContentGuidelineApprovalBulkActionSerializer
+    
+    @transaction.atomic()
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
+    
+
+class ContentGuidelinesBulkApprovalViewset(GenericViewSet, CreateModelMixin):
+    permission_classes = [IsReviewer]
+    serializer_class = ContentGuidelineApprovalBulkActionSerializer
+    http_method_names = ['post']
     
     @transaction.atomic()
     def perform_create(self, serializer):
