a83aa2c3ee63d8158c976fb429da996afe4af5b5
rahuleduva <rahul.eduva@gmail.com>
2024-03-19 11:51:40 +0530

    Code cleanus - Generic api views removed, only viewsets retained

--- content_management/urls.py
+++ content_management/urls.py
@@ -19,14 +19,5 @@ router.register(r'contents', ContentViewSet, basename='content')
 router.register(r'contentGuidelinesBulkApprovalsViewset', ContentGuidelinesBulkApprovalViewset, basename='contentGuidelinesBulkApproval')
 
 urlpatterns = [
-    path('listCreateGuideline/', ListCreateGuideline.as_view(), name='listCreateGuideline'),
-    path('retrieveUpdateDeleteGuideline/<int:pk>/', RetrieveUpdateDestroyGuideline.as_view(), name='retrieveUpdateDeleteGuideline'),
-    path('listCreateContent/', ListCreateContent.as_view(), name='listCreateContent'),
-    path('retrieveUpdateDeleteContent/<int:pk>', RetrieveUpdateDestroyContent.as_view(), name='retrieveUpdateDeleteContent'),
-    path('contentGuidelineBulkApproval/', ContentGuidelineBulkApproval.as_view(), name='contentGuidelineBulkApproval'),
-    path('contentGuidelinesBulkApprovalV2/', ContentGuidelinesBulkApprovalV2.as_view(), name='contentGuidelinesBulkApprovalV2'),
-]
-
-urlpatterns += [
     path('', include(router.urls)),
 ]
